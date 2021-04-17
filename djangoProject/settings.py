a18128ef4a305d71f97dae396951326d0c653287
Pagante <55301195+Pagante@users.noreply.github.com>
2021-04-17 21:03:57 +0100

    allowed host

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dp0m-md^lfg6!ln)q^#z97f=!si+ge*=78#zj2-adi_80audj6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
